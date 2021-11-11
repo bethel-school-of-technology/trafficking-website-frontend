16be5d8e790eace63804f257c2ca25314234924e
James Reisenberg Jr <reisenbergjames@gmail.com>
2021-11-11 14:35:01 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HotlineComponent } from './hotline/hotline.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { InfoComponent } from './info/info.component';
+import { ConnectComponent } from './connect/connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HotlineComponent,
+    TestimonialsComponent,
+    StatisticsComponent,
+    InfoComponent,
+    ConnectComponent
   ],
   imports: [
     BrowserModule,
