639fc75e129d15dd8d1b8111dd35c7c6a62779cf
Jggilbert <juliana.gilbert.bst@visible.edu>
2021-11-30 22:46:05 -0600

    added organizations page, can't get to it though, will work on routing next

--- src/app/Pages/Public/organizations/organizations.component.ts
+++ src/app/Pages/Public/organizations/organizations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Organization } from 'Models/organization';
+import { OrganizationsService } from 'src/app/Services/organizations.service';
 
 @Component({
   selector: 'app-organizations',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationsComponent implements OnInit {
 
-  constructor() { }
+ listOfOrganizations: Organization[] = [];
 
+
+  constructor(private myOrganizationsService: OrganizationsService) { }
+
+  // happens right away
   ngOnInit(): void {
+    // using the serive function that you made called getAllOrganizations and recieving a response
+    this.myOrganizationsService.getAllOrganizations().subscribe(response => {
+      console.log(response);
+      this.listOfOrganizations = response;
+    })
   }
 
 }
